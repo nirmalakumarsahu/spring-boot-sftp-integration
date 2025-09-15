cf858b22279d32ebc6335ce67ad8eed136ffcc77
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-09-15 23:00:51 +0530

    initial Commit add spring project code

---
+++ src/main/java/com/sahu/springboot/basics/SpringBootSftpIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.sahu.springboot.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSftpIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSftpIntegrationApplication.class, args);
+	}
+
+}
