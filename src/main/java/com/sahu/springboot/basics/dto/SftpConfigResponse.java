b29cc2714c477b9cc942431da85d5025866d0b08
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-09-16 07:34:03 +0530

    Update SftpConfigResponse.java

--- src/main/java/com/sahu/springboot/basics/dto/SftpConfigResponse.java
+++ src/main/java/com/sahu/springboot/basics/dto/SftpConfigResponse.java
@@ -1,10 +1,12 @@
 package com.sahu.springboot.basics.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.sahu.springboot.basics.constant.AuthenticationType;
 import com.sahu.springboot.basics.constant.KeyFormat;
 import lombok.Builder;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record SftpConfigResponse(
         Long id,
         String name,
